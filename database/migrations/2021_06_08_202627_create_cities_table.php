e68cb776da6fb959c2927b8880375529a2345c20
Ahmed Abdullah <ahmed01799@gmail.com>
2021-06-12 15:32:33 +0300

    city commit

--- database/migrations/2021_06_08_202627_create_cities_table.php
+++ database/migrations/2021_06_08_202627_create_cities_table.php
@@ -14,6 +14,9 @@ class CreateCityTable extends Migration
     public function up()
     {
         Schema::create('cities', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->timestamps();
         });
     }
 
